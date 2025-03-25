b9663891279e5456a61e2db1c3a92a61932869d5
svanyo03 <svanyo@albany.edu>
2025-03-24 20:10:12 -0400

    Add files via upload

---
+++ March 24th 2.0/output.js
@@ -0,0 +1,15 @@
+function showInnerHTML(){
+      document.getElementById("output1").innerHTML = "Hello from a galaxy far far away!";
+
+
+function useDocumentWrite(){
+      document.write("Self Destruct Initiated");
+}
+
+function showAlert(){
+      alert("Beep Boop Beep ALERT ALERT!");
+}
+
+function showConsole(){
+      console.log("Welcome to the Console ;)");
+}
